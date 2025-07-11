8f3d366233991ea66527e14bfbbc44dbfaa5f93f
unknown <a.shataie2023@gmail.com>
2025-07-10 22:58:18 -0700

    feat: Create socket connection in client

--- client.py
+++ client.py
@@ -1,7 +1,16 @@
 import socket, subprocess, os, base64
 
-def run():
-    pass
+
+def run(host, port):
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            client_socket.connect((host, port))
+        except:
+            return
+    except:
+        return
+
 
 if "__main__" == __name__:
     run()
