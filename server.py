928ff3996df6b3924c507b958621975259f0cbb6
unknown <a.shataie2023@gmail.com>
2025-07-11 23:25:58 -0700

    feat: Run server on all IPs

--- server.py
+++ server.py
@@ -2,6 +2,9 @@ import socket, base64
 from termcolor import colored
 
 
-def run_server():
+def run_server(host, port):
     pass
 
+
+if "__main__" == __name__:
+    run_server(host="0.0.0.0", port=6018)
