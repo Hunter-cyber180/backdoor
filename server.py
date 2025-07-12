a68b1eb3134d4e5a9211a5df7e1eb1e563a7c2d2
unknown <a.shataie2023@gmail.com>
2025-07-11 23:28:39 -0700

    feat: Set options for server socket

--- server.py
+++ server.py
@@ -4,7 +4,9 @@ from termcolor import colored
 
 def run_server(host, port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print(colored("[+] Server Socket Created.", "blue"))
+
 
 
 if "__main__" == __name__:
