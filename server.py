b2775b99e9f48ab28af6b0acd3f73a55b6f66414
unknown <a.shataie2023@gmail.com>
2025-07-11 23:32:07 -0700

    feat: Listen for incoming connections (max 5 pending connection)

--- server.py
+++ server.py
@@ -7,7 +7,8 @@ def run_server(host, port):
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     print(colored("[+] Server Socket Created.", "blue"))
     server_socket.bind((host, port))
-
+    server_socket.listen(5)
+    print(colored("[+] Server Is Listening...", "blue"))
 
 if "__main__" == __name__:
     run_server(host="0.0.0.0", port=6018)
